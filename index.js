61108734ea24c00a47a0d4c4dacdd7cd9f8e578f
Micael Gomez <mikael85@gmail.com>
2020-10-10 16:06:19 -0300

    [ADD] Get Coupons Coupons are separated

--- index.js
+++ index.js
@@ -43,6 +43,105 @@ const fillFrm = async (currentPosPage) => {
   return;
 };
 
+const _get_raw_coupon = (id) => {
+  const url = `https://ms.mobbex.com/prod/reports/v2/coupon/${id}?schema=card&format=html`;
+  const coupon = new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      var body = '';
+      response.on('data', (chunk) => {
+        body += chunk;
+      });
+      response.on('end', () => {
+        body = body.replace(/\s{2,}/g, '').trim();
+        resolve({ 'result': true, 'data': body, 'error': null });
+      });
+    }).on('error', (e) => {
+      console.error('Error: ', e);
+      reject({ 'result': false, 'data': null, 'error': e });
+    });
+  });
+  return coupon;
+};
+
+const _get_base64_qr = (img_url) => {
+  const image = new Promise((resolve, reject) => {
+    https.get(img_url, (response) => {
+      response.setEncoding('base64');
+      var body = "data:" + response.headers["content-type"] + ";base64,";
+      response.on('data', (chunk) => {
+        body += chunk;
+      });
+      response.on('end', () => {
+        resolve({ 'result': true, 'data': body, 'error': null });
+      });
+    }).on('error', (e) => {
+      resolve({ 'result': false, 'data': null, 'error': e });
+      reject(e);
+    });
+  });
+  return image;
+};
+
+const b2a = (a) => {
+  var c, d, e, f, g, h, i, j, o, b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", k = 0, l = 0, m = "", n = [];
+  if (!a) return a;
+  do c = a.charCodeAt(k++), d = a.charCodeAt(k++), e = a.charCodeAt(k++), j = c << 16 | d << 8 | e,
+    f = 63 & j >> 18, g = 63 & j >> 12, h = 63 & j >> 6, i = 63 & j, n[l++] = b.charAt(f) + b.charAt(g) + b.charAt(h) + b.charAt(i); while (k < a.length);
+  return m = n.join(""), o = a.length % 3, (o ? m.slice(0, o - 3) : m) + "===".slice(o || 3);
+};
+
+const get_coupon = async (transaction_id) => {
+  var raw_coupon = false;
+  while (!raw_coupon) {
+    try {
+      raw_coupon = await _get_raw_coupon(transaction_id);
+      if (raw_coupon.result) {
+        raw_coupon = raw_coupon.data;
+      }
+    } catch (e) {
+      console.error(e);
+      raw_coupon = false;
+    }
+  }
+  var base64qr = false;
+  raw_coupon = await raw_coupon.replace(/<meta[ a-zA-Z"-=]*\/>/g, ''); // remove <meta/> tags
+  raw_coupon = await raw_coupon.replace(/<script[ a-zA-Z"-=]*\/?>[ a-zA-Z.=();{}]*<\/script>/g, ''); //remove <script/> tag and content
+
+  var html_body = await raw_coupon.match(/(<body>(<div class="col".*<\/div><\/div>)(<div class="col".*<\/div><\/div>)<\/body>)/);
+  var signature_coupon = html_body[2];
+  var qr_coupon = html_body[3];
+
+
+  image_tag = await raw_coupon.match(/(<img .* src="(.*)" \/>)/); //replace img link with base64, so it can be sent to jposbox
+  //image_tag: [0]: whole match, [1]: whole img tag, [2]: image link, must be passed to _get_base64_qr
+  while (!base64qr) {
+    try {
+      base64qr = await _get_base64_qr(image_tag[2]);
+      if (base64qr.result) {
+        base64qr = base64qr.data;
+      }
+
+    } catch (e) {
+      console.log(e);
+      base64qr = false;
+    }
+  }
+
+  const new_image_tag = `<img width="120" height="120" src="${base64qr}"/>`;
+  qr_coupon = await qr_coupon.replace(/(<img .* src="(.*)" \/>)/g, new_image_tag);
+  
+  const coupon1 = await raw_coupon.replace(/(<body>(<div class="col".*<\/div><\/div>)(<div class="col".*<\/div><\/div>)<\/body>)/g, `<body>${signature_coupon}</body>`)
+    .replace('<div class="col"', '<div class="row"');
+
+  const coupon2 = await raw_coupon.replace(/(<body>(<div class="col".*<\/div><\/div>)(<div class="col".*<\/div><\/div>)<\/body>)/g, `<body>${qr_coupon}</body>`)
+    .replace('<div class="col"', '<div class="row"');
+
+  return {
+    'mobbex_coupon': coupon1,
+    'mobbex_qr_coupon': coupon2,
+  };
+};
+
 const main = async () => {
   const browser = await puppeteer.connect({ browserURL, defaultViewport });
   const pages = await browser.pages();
@@ -64,6 +163,18 @@ const main = async () => {
   // Expose a handler to the page
   await posPage.exposeFunction('onCardSwiped', cardData => {
     cardSwipedData = cardData;
+  });
+
+  //TODO: Save first request to avoid process two times
+  await posPage.exposeFunction('printMobbexCoupon', async transaction_id => {
+    const coupon = await get_coupon(transaction_id).mobbex_coupon;
+    // Maniputate posPage to set content
+  });
+
+  await posPage.exposeFunction('printMobbexQRCoupon', async transaction_id => {
+    const coupon = await get_coupon(transaction_id).mobbex_qr_coupon;
+    // Maniputate posPage to set content
+
   });
 
 };
--- test.js
+++ test.js
@@ -1,45 +1,116 @@
 const puppeteer = require('puppeteer-core');
+const https = require('https');
 
-// Must start Chrome with flags: 
-// --remote-debugging-port=21222 
-// --disable-web-security
-// --disable-features=IsolateOrigins,site-per-process 
+const _get_raw_coupon = (id) => {
+    const url = `https://ms.mobbex.com/prod/reports/v2/coupon/${id}?schema=card&format=html`;
+    const coupon = new Promise((resolve, reject) => {
+        https.get(url, (response) => {
+            var body = '';
+            response.on('data', (chunk) => {
+                body += chunk;
+            });
+            response.on('end', () => {
+                body = body.replace(/\s{2,}/g, '').trim();
+                resolve({ 'result': true, 'data': body, 'error': null });
+            });
+        }).on('error', (e) => {
+            console.error('Error: ', e);
+            reject({ 'result': false, 'data': null, 'error': e });
+        });
+    });
+    return coupon;
+};
 
-// One and only one Odoo POS Tab
-// https://stackoverflow.com/questions/55096771/connecting-browsers-in-puppeteer
-// https://stackoverflow.com/questions/46529201/puppeteer-how-to-fill-form-that-is-inside-an-iframe
+const _get_base64_qr = (img_url) => {
+    const image = new Promise((resolve, reject) => {
+        https.get(img_url, (response) => {
+            response.setEncoding('base64');
+            var body = "data:" + response.headers["content-type"] + ";base64,";
+            response.on('data', (chunk) => {
+                body += chunk;
+            });
+            response.on('end', () => {
+                resolve({ 'result': true, 'data': body, 'error': null });
+            });
+        }).on('error', (e) => {
+            resolve({ 'result': false, 'data': null, 'error': e });
+            reject(e);
+        });
+    });
+    return image;
+};
 
-const browserURL = 'http://127.0.0.1:21222'; // you should start chrome with --remote-debugging-port=21222
-const defaultViewport = null; // Disables 800 * 600 default
+// function b2a(a) {
+const b2a = (a) => {
+    var c, d, e, f, g, h, i, j, o, b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", k = 0, l = 0, m = "", n = [];
+    if (!a) return a;
+    do c = a.charCodeAt(k++), d = a.charCodeAt(k++), e = a.charCodeAt(k++), j = c << 16 | d << 8 | e,
+        f = 63 & j >> 18, g = 63 & j >> 12, h = 63 & j >> 6, i = 63 & j, n[l++] = b.charAt(f) + b.charAt(g) + b.charAt(h) + b.charAt(i); while (k < a.length);
+    return m = n.join(""), o = a.length % 3, (o ? m.slice(0, o - 3) : m) + "===".slice(o || 3);
+};
 
 const main = async () => {
-    const browser = await puppeteer.connect({ browserURL, defaultViewport });
+    const hcid = 'KlYknhJn2'; //hardcoded
+    var raw_coupon = false;
+    while (!raw_coupon) {
+        try {
+            raw_coupon = await _get_raw_coupon(hcid);
+            if (raw_coupon.result) {
+                raw_coupon = raw_coupon.data;
+            }
+        } catch (e) {
+            console.error(e);
+            raw_coupon = false;
+        }
+    }
+    var base64qr = false;
+    // remove <meta/> tags
+    raw_coupon = raw_coupon.replace(/<meta[ a-zA-Z"-=]*\/>/g, '');
+    //remove <script/> tag and content
+    raw_coupon = raw_coupon.replace(/<script[ a-zA-Z"-=]*\/?>[ a-zA-Z.=();{}]*<\/script>/g, '');
+    var html_body = await raw_coupon.match(/(<body>(<div class="col".*<\/div><\/div>)(<div class="col".*<\/div><\/div>)<\/body>)/);
+    var signature_coupon = html_body[2];
+    var qr_coupon = html_body[3];
 
-    const pages = await browser.pages();
-    let posPage = false;
-    let cardSwipedData = false;
+    //replace img link with base64, so it can be sent to jposbox
+    // DO NOT SET /g ON REGEX, it won't be showing capturing groups
+    image_tag = await raw_coupon.match(/(<img .* src="(.*)" \/>)/);
+    //image_tag[0]: whole match
+    //image_tag[1]: whole img tag
+    //image_tag[2]: image link, must be passed to _get_base64_qr
+    console.log('image_tag[2]:', image_tag[2]);
+    while (!base64qr) {    
+        try {
+            base64qr = await _get_base64_qr(image_tag[2]);
+            if (base64qr.result) {
+                base64qr = base64qr.data;
+            }
+        } catch (e) {
+            console.log(e);
+            base64qr = false;
+        }
+    }
+    // base64qr = await _get_base64_qr(image_tag[2]);
+    var new_image_tag = `<img width="120" height="120" src="${base64qr}"/>`;
+    qr_coupon = qr_coupon.replace(/(<img .* src="(.*)" \/>)/g, new_image_tag);
 
-    let mbbxFrm = false;
-    let mbbxFrmFilled = false;
+    const coupon1 = raw_coupon.replace(/(<body>(<div class="col".*<\/div><\/div>)(<div class="col".*<\/div><\/div>)<\/body>)/g, `<body>${signature_coupon}</body>`)
+        .replace('<div class="col"', '<div class="row"');
+        // .replace('<body>', '<body style="flex-direction: column;">');
+    const coupon2 = raw_coupon.replace(/(<body>(<div class="col".*<\/div><\/div>)(<div class="col".*<\/div><\/div>)<\/body>)/g, `<body>${qr_coupon}</body>`)
+        .replace('<div class="col"', '<div class="row"');
+        // .replace('<body>', '<body style="flex-direction: column;">');
 
-    // Search for the first ocurrence of POS Tab Page
-    posPage = pages.find(page => page.url().indexOf('/pos/web/') > -1);
 
-    // handle iframe
-    const frameHandle = await posPage.$('#mbbx-container iframe:nth-child(2)');
-    mbbxFrm = await frameHandle.contentFrame();
-
-    // filling form in iframe
-    // await frame.type('input', 'chercher tech');
-    
-    // mbbxFrm = await posPage.mainFrame().childFrames().find(frame => frame.name().indexOf('mobbex_embed_fw')>-1);
-
-    // await mbbxFrm.click('#payment-methods-single');
-    await mbbxFrm.$eval('#payment-methods-single', el => el.click());
-    // await mbbxFrm.type('#cc-number', '4894121721340129');
-
+    // console.log('raw_coupon: ', raw_coupon);
+    // console.log('image_tag: ', image_tag);
+    // console.log('coupon1: ', b2a(coupon1));
+    console.log('coupon1: ', coupon1);
+    console.log('coupon2: ', coupon2);
+    // console.log('coupon2: ', b2a(coupon2));
+    // console.log('coupon: ', coupon);
+    // console.log('coupon: ', raw_coupon);
 
 };
 
 main();
-
